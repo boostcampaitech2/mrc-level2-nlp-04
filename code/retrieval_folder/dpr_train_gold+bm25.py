e6557147e13be53a761ecd4566c70fbc32878cda
kimziont <ziontkim0510@gmail.com>
2021-10-21 23:21:28 +0000

    Update commit

--- code/retrieval_folder/dpr_train_gold+bm25.py
+++ code/retrieval_folder/dpr_train_gold+bm25.py
@@ -177,8 +177,8 @@ args = TrainingArguments(
     output_dir="dense_retrieval",
     evaluation_strategy="epoch",
     learning_rate=2e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
     num_train_epochs=5,
     weight_decay=0.01
 )
